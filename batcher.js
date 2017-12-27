b1c8fb25ae0470d34cb4da30cd13e10dbf881cc3
Juan Lledo <juan.lledofrasquet@student.manchester.ac.uk>
2017-12-27 22:29:00 +0100

    Initial commit

---
+++ batcher.js
@@ -0,0 +1,59 @@
+var DRY_RUN = false;
+var resList = [480, 720, 1080];
+var downloadPath = "http://localhost:8080/command/download";
+
+function post(path, params, callback) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", path, true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4)
+            callback(xhr);
+    };
+
+    let formData = new FormData();
+    for (let key in params) {
+        formData.append(key, params[key]);
+    }
+    xhr.send(formData);
+}
+
+function downloadVisibleLinks(res) {
+    let magnets = {};
+    magnets[res] = document.querySelectorAll("div[class$='" + res + "p'] a[title='Magnet Link']");
+
+    if (!DRY_RUN) {
+        magnets[res].forEach(magnet => post(downloadPath, {"urls": magnet.href}, (xhr) => console.log(xhr.response)));
+    } else {
+        console.log("Magnets found for " + res + "p: " + magnets[res].length);
+    }
+}
+
+function injectElements() {
+    let parent = document.querySelector(".episodecontainer");
+
+    let div = document.createElement("div");
+    div.setAttribute("class", "hs-batcher");
+    parent.insertBefore(div, parent.childNodes[2]);
+
+    let wrapper = document.createElement("div");
+    wrapper.style.textAlign = "center";
+    div.append(wrapper);
+
+    let select = document.createElement("select");
+    for (let i = 0; i < resList.length; i++) {
+        let option = document.createElement("option");
+        option.value = resList[i];
+        option.innerHTML = resList[i] + "p";
+        select.append(option);
+    }
+    wrapper.append(select);
+
+    let button = document.createElement("button");
+    button.innerHTML = "Download";
+    button.onclick = () => downloadVisibleLinks(select.options[select.selectedIndex].value);
+    wrapper.append(button);
+}
+
+injectElements();
