2f048351d6bb02880098e634fbfb031ca5d0f603
Juan Lledo <juan.lledo@hotmail.com>
2020-03-22 02:52:33 +0100

    Add Web API URL customisation

---
+++ options/options.js
@@ -0,0 +1,21 @@
+function saveOptions(e) {
+    let formData = new FormData(e.srcElement);
+    browser.storage.sync.set({
+        host: formData.get("host"),
+        port: formData.get("port"),
+    });
+    e.preventDefault();
+}
+
+async function restoreOptions() {
+    let defaultsURL = browser.runtime.getURL("options/defaults.json");
+    let response = await fetch(defaultsURL, { mode: "no-cors" });
+    let defaults = await response.json();
+
+    let settings = await browser.storage.sync.get(defaults);
+    document.getElementById("host").value = settings.host;
+    document.getElementById("port").value = settings.port;
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
